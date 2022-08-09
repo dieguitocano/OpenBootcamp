cca6b3281f5be160f1746daacc9503c0f89f3814
dieguitocano <dieguito.kano@gmail.com>
2022-08-09 09:30:34 -0500

    Segunda entrega

---
+++ SegundaEntrega/data.js
@@ -0,0 +1,13 @@
+const Diego = {
+    nombre: "Diego",
+    edad: 31,
+    desarrollador: true,
+    date: "03-06-1990"
+    libro = {
+        titulo: "Libro de Oro",
+        autor: "Saint Germain",
+        fecha: "edicion 1970",
+        url: null
+    }
+
+}
